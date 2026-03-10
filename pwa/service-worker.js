8bae03c3e96d24750a4d00134154c722f598f726
DrakeJungblud <132248375+DrakeJungblud@users.noreply.github.com>
2026-03-09 23:34:01 -0700

    Update service-worker.js

--- pwa/service-worker.js
+++ pwa/service-worker.js
@@ -1,1 +1,20 @@
-// service worker placeholder 
+//const CACHE_NAME = 'vj-studio-v1';
+const ASSETS = [
+'../index.html',
+'../modules/beDucky/ducky.js',
+'../modules/beDucky/ducky.css',
+'../pwa/manifest.webmanifest',
+'../ducky.png'
+];
+
+self.addEventListener('install', (e) => {
+e.waitUntil(
+caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+);
+});
+
+self.addEventListener('fetch', (e) => {
+e.respondWith(
+caches.match(e.request).then(res => res || fetch(e.request))
+);
+}); service worker placeholder 
